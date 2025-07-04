0a8375bc9dd611472de6ccab2fdcaa2e4f8889ad
Mahdieh <Mahdiehkhajuie1382@gmail.com>
2025-07-04 19:38:51 +0200

    ADD :project added

---
+++ Assets/Scripts/DropletBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DropletBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+
+    public void SpawnDroplet()
+    {
+        Instantiate(_prefab, new Vector3(Random.Range(-8f, 8f), 9f, 0), Quaternion.identity );
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D other )
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.TakeDamage(1); // کم کردن سلامتی
+            GameManager.Instance.AddScore(1);  // افزایش امتیاز 
+           SpawnDroplet();
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            GameManager.Instance.MissedDrop();
+            Destroy(gameObject);
+        }
+    
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int score = 0;
+    public int health = 3;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
+
+    public int maxMissedDrops = 15;
+    private int missedDrops = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateUI();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        UpdateUI();
+
+        if (health <= 0)
+        {
+            Debug.Log("You Lost!");
+        }
+    }
+
+    public void MissedDrop()
+    {
+        missedDrops++;
+        Debug.Log("Missed Drops: " + missedDrops);
+
+        if (missedDrops >= maxMissedDrops)
+        {
+            Debug.Log("You Lost!");
+        }
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        healthText.text = "Health: " + health;
+    }
+}
